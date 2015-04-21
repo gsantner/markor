4a4db5015b7ef8bdcc8ca61a231aa8490670ec85
Jeff Martin <jffrymrtn1@gmail.com>
2015-04-20 22:26:12 -0400

    fab works in widget

--- app/src/main/java/me/writeily/pro/widget/WriteilyWidgetProvider.java
+++ app/src/main/java/me/writeily/pro/widget/WriteilyWidgetProvider.java
@@ -1,9 +1,36 @@
 package me.writeily.pro.widget;
 
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import me.writeily.pro.NoteActivity;
+import me.writeily.pro.R;
 
 /**
  * Created by jeff on 15-02-04.
  */
 public class WriteilyWidgetProvider extends AppWidgetProvider {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i = 0; i < N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, NoteActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+            views.setOnClickPendingIntent(R.id.widget_new_note, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
 }
