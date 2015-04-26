7e4d8decfc1f11041db37265da6297fcbc1fc9cb
plafue <pedro.lafuente@gmx.de>
2015-04-26 19:39:13 +0200

    Added the possibility to use alphanumeric passcodes. Closes #49

---
+++ app/src/main/java/me/writeily/pro/PromptPinActivity.java
@@ -0,0 +1,50 @@
+package me.writeily.pro;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import me.writeily.pro.model.Constants;
+
+/**
+ * Created by jeff on 2014-08-20.
+ */
+public class PromptPinActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the Intent (to check if coming from Settings)
+        String action = getIntent().getAction();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_nonelevated);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+        // Get the pin a user may have set
+        String[] stringArray = getResources().getStringArray(R.array.possibleLocksValues);
+        String lockType = PreferenceManager.getDefaultSharedPreferences(this).getString(getString(R.string.pref_lock_type_key), "");
+        if (lockType == null || lockType.equals("") || stringArray[0].equals(lockType)) {
+            Intent mainIntent = new Intent(this, MainActivity.class);
+            startActivity(mainIntent);
+            this.finish();
+        } else if (stringArray[1].equals(lockType)) {
+            Intent pinIntent = new Intent(this, PinActivity.class);
+            startActivity(pinIntent);
+            this.finish();
+        } else if (stringArray[2].equals(lockType)) {
+            Intent pinIntent = new Intent(this, AlphanumericPinActivity.class);
+            startActivity(pinIntent);
+            this.finish();
+        }
+    }
+}
