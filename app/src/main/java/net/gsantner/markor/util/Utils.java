07096eb44e1d301d03d29e84c272454ec5424987
Gregor Santner <gsantner@mailbox.org>
2017-09-18 12:42:52 +0200

    Respect theme in RenameDialog

--- app/src/main/java/net/gsantner/markor/util/Utils.java
+++ app/src/main/java/net/gsantner/markor/util/Utils.java
@@ -10,6 +10,7 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 public class Utils {
     private static int screenWidth = 0;
@@ -42,10 +43,17 @@ public class Utils {
 
         return screenWidth;
     }
+
     @SuppressWarnings("ResultOfMethodCallIgnored")
     public static boolean renameFileInSameFolder(File srcFile, String destFilename, String cacheDir) {
         File destFile = new File(srcFile.getParent(), destFilename);
-        File cacheFile = new File(cacheDir, "rename.md");
+        Random random = new Random();
+        File cacheFile;
+
+        // Move file temporary, otherwise "hello.txt"->"heLLo.txt" will not work (file exist)
+        do {
+            cacheFile = new File(cacheDir, random.nextInt() + "rename.tmp");
+        } while (cacheFile.exists());
         try {
             FileUtils.moveFile(srcFile, cacheFile);
             FileUtils.moveFile(cacheFile, destFile);
