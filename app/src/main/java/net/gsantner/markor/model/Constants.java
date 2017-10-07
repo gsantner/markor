3270c4e70b0d5d26e889ad45f80bd68d1eede277
KrishAmal <amal.aug95@gmail.com>
2017-10-07 12:55:16 +0530

    Implemented Alert Dialog for Link and Image Actions

--- app/src/main/java/net/gsantner/markor/model/Constants.java
+++ app/src/main/java/net/gsantner/markor/model/Constants.java
@@ -18,12 +18,11 @@ public class Constants {
             {R.drawable.format_header_2,2},{R.drawable.format_header_3,3},{R.drawable.format_list_bulleted,4},
             {R.drawable.format_list_numbers,5}};
 
-    public static final String[] KEYBOARD_SMART_ACTIONS = {"**", "_", "~~", "`"};
+    public static final String[] KEYBOARD_SMART_ACTIONS = {"**", "_", "~~", "`", "----\n"};
     public static final int[][] KEYBOARD_SMART_ACTIONS_ICON =  {{R.drawable.format_bold,0},{R.drawable.format_italic,1},
-            {R.drawable.format_strikethrough,2},{R.drawable.format_code,3}};
+            {R.drawable.format_strikethrough, 2}, {R.drawable.format_code, 3}, {R.drawable.format_horizontal_line, 4}};
 
-    public static final String[] KEYBOARD_EXTRA_ACTIONS = {"----\n","[ ]( ) "};
-    public static final int[][] KEYBOARD_EXTRA_ACTIONS_ICONS ={{R.drawable.format_horizontal_line,0},{R.drawable.format_link,1}};
+    public static final int[][] KEYBOARD_EXTRA_ACTIONS_ICONS = {{R.drawable.format_link, 1}, {R.drawable.format_image, 2}};
 
     public static final String UTF_CHARSET = "utf-8";
 
