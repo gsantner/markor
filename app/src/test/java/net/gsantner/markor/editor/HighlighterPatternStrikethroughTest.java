4cd2e389dfe882eea1b25d2f77c827f45069c5d9
Jamie T <30901127+jamiet-dev@users.noreply.github.com>
2017-10-15 21:09:04 -0300

    Rework Highlighter (#78)
    Rework Highlighter

---
+++ app/src/test/java/net/gsantner/markor/editor/HighlighterPatternStrikethroughTest.java
@@ -0,0 +1,70 @@
+package net.gsantner.markor.editor;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HighlighterPatternStrikethroughTest {
+
+    private Pattern pattern;
+
+    @Before
+    public void before() {
+        pattern = HighlighterPattern.STRIKETHROUGH.getPattern();
+    }
+
+    @Test
+    public void strikethrough() {
+        Matcher m = pattern.matcher("~~strikethrough~~");
+        assertThat(m.find()).isTrue();
+        assertThat(m.group()).isEqualTo("~~strikethrough~~");
+    }
+
+    @Test
+    public void strikethroughSingleCharacter() {
+        Matcher m = pattern.matcher("~~s~~");
+        assertThat(m.find()).isTrue();
+        assertThat(m.group()).isEqualTo("~~s~~");
+    }
+
+    @Test
+    public void strikethroughWithSpace() {
+        Matcher m = pattern.matcher("~~strike through~~");
+        assertThat(m.find()).isTrue();
+        assertThat(m.group()).isEqualTo("~~strike through~~");
+    }
+
+    @Test
+    public void multipleStrikethrough() {
+        Matcher m = pattern.matcher("~~one~~ ~~two~~");
+        assertThat(m.find()).isTrue();
+        assertThat(m.group()).isEqualTo("~~one~~");
+        assertThat(m.find()).isTrue();
+        assertThat(m.group()).isEqualTo("~~two~~");
+    }
+
+    @Test
+    public void strikethroughWithPrecedingTilde() {
+        Matcher m = pattern.matcher("~~~one~~");
+        assertThat(m.find()).isTrue();
+        assertThat(m.group()).isEqualTo("~~~one~~");
+    }
+
+    @Test
+    public void strikethroughWithTrailingTildeSkipsExtraTildes() {
+        Matcher m = pattern.matcher("~~one~~~");
+        assertThat(m.find()).isTrue();
+        assertThat(m.group()).isEqualTo("~~one~~");
+    }
+
+    @Test
+    public void strikethroughWithSpaceAsLastCharacherShouldNotMatch() {
+        Matcher m = pattern.matcher("~~one ~~");
+        assertThat(m.find()).isFalse();
+    }
+
+}
