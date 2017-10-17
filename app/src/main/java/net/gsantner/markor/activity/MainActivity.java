b59a044dbf03f22506e362a1f4341e661285f762
Manmeet Singh <manmeet-22@users.noreply.github.com>
2017-10-18 01:28:53 +0530

    [WIP] Extension Bug Fix (#77)
    * Now supports multiple extension
    
    * Update Constants.java

--- app/src/main/java/net/gsantner/markor/activity/MainActivity.java
+++ app/src/main/java/net/gsantner/markor/activity/MainActivity.java
@@ -51,7 +51,6 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.OnLongClick;
 
-
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
