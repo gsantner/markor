eda2c69a110401383bfee5b6964baf1f8c92e106
Gregor Santner <gsantner@mailbox.org>
2017-10-30 12:38:27 +0100

    Added gplay dialog

--- app/src/main/java/net/gsantner/opoc/util/ContextUtils.java
+++ app/src/main/java/net/gsantner/opoc/util/ContextUtils.java
@@ -54,7 +54,6 @@ import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
-import android.text.style.ForegroundColorSpan;
 import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -543,5 +542,19 @@ public class ContextUtils {
             }
         }
         return this;
+    }
+
+    public void showRateOnGplayDialog() {
+        String pkgId = "details?id=" + _context.getPackageName();
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, Uri.parse("market://" + pkgId));
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                (Build.VERSION.SDK_INT >= 21 ? Intent.FLAG_ACTIVITY_NEW_DOCUMENT : Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET) |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        try {
+            _context.startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            _context.startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/" + pkgId)));
+        }
     }
 }
