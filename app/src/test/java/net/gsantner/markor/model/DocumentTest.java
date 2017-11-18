89d7cb433b4b9ac7147cb6a422811a2f9512d8b8
Gregor Santner <gsantner@mailbox.org>
2017-11-19 00:34:59 +0100

    Disallow path characters as input as document title

--- app/src/test/java/net/gsantner/markor/model/DocumentTest.java
+++ app/src/test/java/net/gsantner/markor/model/DocumentTest.java
@@ -11,10 +11,9 @@
  */
 package net.gsantner.markor.model;
 
-import net.gsantner.markor.model.Document;
-
 import org.junit.Test;
 
+import static net.gsantner.markor.util.DocumentIO.normalizeTitleForFilename;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DocumentTest {
@@ -47,5 +46,25 @@ public class DocumentTest {
         document.goToNewerVersion();
         assertThat(document.getTitle()).isEqualTo("Hello World Again");
         assertThat(document.canGoToNewerVersion()).isEqualTo(false);
+    }
+
+    @Test
+    public void filenameNormalization(){
+        assertThat(normalizeTitleForFilename(nd(null, "HelloWorld"))).isEqualTo("HelloWorld");
+        assertThat(normalizeTitleForFilename(nd("HelloWorld", "text"))).isEqualTo("HelloWorld");
+        assertThat(normalizeTitleForFilename(nd(null, "text\nnewline"))).isEqualTo("text");
+        assertThat(normalizeTitleForFilename(nd(null, "sumtext/folder"))).isEqualTo("sumtextfolder");
+        assertThat(normalizeTitleForFilename(nd(null, "## hello world"))).isEqualTo("hello world");
+    }
+
+    private Document nd(String title, String content){
+        Document document = new Document();
+        if (title != null){
+            document.setTitle(title);
+        }
+        if (content != null){
+            document.setContent(content);
+        }
+        return document;
     }
 }
