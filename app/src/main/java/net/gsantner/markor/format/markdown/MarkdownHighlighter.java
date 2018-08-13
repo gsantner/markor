455e65354bbacf2636412662a8a9669464f1e7d7
Gregor Santner <gsantner@mailbox.org>
2018-08-13 18:43:16 +0200

    Improve code block color (#295) and minor markdown syntax highlight improvements

--- app/src/main/java/net/gsantner/markor/format/markdown/MarkdownHighlighter.java
+++ app/src/main/java/net/gsantner/markor/format/markdown/MarkdownHighlighter.java
@@ -23,16 +23,20 @@ import net.gsantner.markor.util.AppSettings;
 import other.writeily.format.markdown.MarkdownHeaderSpanCreator;
 
 public class MarkdownHighlighter extends Highlighter {
-    private final MarkdownHighlighterColors _colors;
     public final String _fontType;
     public final Integer _fontSize;
     private final boolean _highlightHexcolorEnabled;
     private final boolean _highlightLineEnding;
     private final boolean _highlightCodeChangeFont;
 
+    private static final int MD_COLOR_HEADER = 0xffef6D00;
+    private static  final int MD_COLOR_LINK = 0xff1ea3fe;
+    private static  final int MD_COLOR_LIST = 0xffdaa521;
+    private static  final int MD_COLOR_QUOTE = 0xff88b04c;
+    private static  final int MD_COLOR_CODEBLOCK = 0xff8c8c8c;
+
     public MarkdownHighlighter() {
         AppSettings as = AppSettings.get();
-        _colors = new MarkdownHighlighterColors();
         _fontType = as.getFontFamily();
         _fontSize = as.getFontSize();
         _highlightHexcolorEnabled = as.isHighlightingHexColorEnabled();
@@ -51,34 +55,34 @@ public class MarkdownHighlighter extends Highlighter {
 
             _profiler.start(true, "Markdown Highlighting");
 
-            _profiler.restart("Header");
-            createHeaderSpanForMatches(editable, MarkdownHighlighterPattern.HEADER, _colors.getHeaderColor());
-            _profiler.restart("Link");
-            createColorSpanForMatches(editable, MarkdownHighlighterPattern.LINK.pattern, _colors.getLinkColor());
-            _profiler.restart("List");
-            createColorSpanForMatches(editable, MarkdownHighlighterPattern.LIST_UNORDERED.pattern, _colors.getListColor());
-            _profiler.restart("OrderedList");
-            createColorSpanForMatches(editable, MarkdownHighlighterPattern.LIST_ORDERED.pattern, _colors.getListColor());
+            //_profiler.restart("Header");
+            createHeaderSpanForMatches(editable, MarkdownHighlighterPattern.HEADER, MD_COLOR_HEADER);
+            //_profiler.restart("Link");
+            createColorSpanForMatches(editable, MarkdownHighlighterPattern.LINK.pattern, MD_COLOR_LINK);
+            //_profiler.restart("List");
+            createColorSpanForMatches(editable, MarkdownHighlighterPattern.LIST_UNORDERED.pattern, MD_COLOR_LIST);
+            //_profiler.restart("OrderedList");
+            createColorSpanForMatches(editable, MarkdownHighlighterPattern.LIST_ORDERED.pattern, MD_COLOR_LIST);
             if (_highlightLineEnding) {
-                _profiler.restart("Double space ending - bgcolor");
-                createColorBackgroundSpan(editable, MarkdownHighlighterPattern.DOUBLESPACE_LINE_ENDING.pattern, _colors.getDoublespaceColor());
+                //_profiler.restart("Double space ending - bgcolor");
+                createColorBackgroundSpan(editable, MarkdownHighlighterPattern.DOUBLESPACE_LINE_ENDING.pattern, MD_COLOR_CODEBLOCK);
             }
-            _profiler.restart("Bold");
+            //_profiler.restart("Bold");
             createStyleSpanForMatches(editable, MarkdownHighlighterPattern.BOLD.pattern, Typeface.BOLD);
-            _profiler.restart("Italics");
+            //_profiler.restart("Italics");
             createStyleSpanForMatches(editable, MarkdownHighlighterPattern.ITALICS.pattern, Typeface.ITALIC);
-            _profiler.restart("Quotation");
-            createColorSpanForMatches(editable, MarkdownHighlighterPattern.QUOTATION.pattern, _colors.getQuotationColor());
-            _profiler.restart("Strikethrough");
+            //_profiler.restart("Quotation");
+            createColorSpanForMatches(editable, MarkdownHighlighterPattern.QUOTATION.pattern, MD_COLOR_QUOTE);
+            //_profiler.restart("Strikethrough");
             createSpanWithStrikeThroughForMatches(editable, MarkdownHighlighterPattern.STRIKETHROUGH.pattern);
             if (_highlightCodeChangeFont) {
-                _profiler.restart("Code - Font [MonoSpace]");
+                //_profiler.restart("Code - Font [MonoSpace]");
                 createMonospaceSpanForMatches(editable, MarkdownHighlighterPattern.CODE.pattern);
             }
-            _profiler.restart("Code - bgolor");
-            createColorBackgroundSpan(editable, MarkdownHighlighterPattern.CODE.pattern, _colors.getDoublespaceColor());
+            //_profiler.restart("Code - bgolor");
+            createColorBackgroundSpan(editable, MarkdownHighlighterPattern.CODE.pattern, MD_COLOR_CODEBLOCK);
             if (_highlightHexcolorEnabled) {
-                _profiler.restart("RGB Color underline");
+                //_profiler.restart("RGB Color underline");
                 createColoredUnderlineSpanForMatches(editable, HexColorCodeUnderlineSpan.PATTERN, new HexColorCodeUnderlineSpan(), 1);
             }
 
