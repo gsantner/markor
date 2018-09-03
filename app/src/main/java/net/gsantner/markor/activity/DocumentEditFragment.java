6a611ebfa9acaa004139426376768b1dbaccec76
kritin <kritinsai1@gmail.com>
2018-09-03 09:38:30 +0530

    optimized imports

--- app/src/main/java/net/gsantner/markor/activity/DocumentEditFragment.java
+++ app/src/main/java/net/gsantner/markor/activity/DocumentEditFragment.java
@@ -21,7 +21,6 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.Menu;
--- app/src/main/java/net/gsantner/markor/format/markdown/MarkdownTextModuleActions.java
+++ app/src/main/java/net/gsantner/markor/format/markdown/MarkdownTextModuleActions.java
@@ -21,7 +21,6 @@ import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
