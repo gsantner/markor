db8da316d2e3f71c8cb2f3f68f19ac041477de81
Radovan Kočík <radovan.kocik@student.tuke.sk>
2018-10-19 18:10:36 +0200

    update

--- app/src/main/java/net/gsantner/markor/format/plaintext/CommonTextModuleActions.java
+++ app/src/main/java/net/gsantner/markor/format/plaintext/CommonTextModuleActions.java
@@ -11,14 +11,26 @@ package net.gsantner.markor.format.plaintext;
 
 import android.app.Activity;
 import android.support.annotation.StringRes;
+import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import net.gsantner.markor.R;
 import net.gsantner.markor.model.Document;
 import net.gsantner.markor.ui.SearchOrCustomTextDialogCreator;
 import net.gsantner.markor.ui.hleditor.HighlightingEditor;
+import net.gsantner.markor.util.DownloadImageTask;
 import net.gsantner.opoc.format.plaintext.PlainTextStuff;
 import net.gsantner.opoc.util.ContextUtils;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class CommonTextModuleActions {
     public static final int ACTION_SPECIAL_KEY__ICON = R.drawable.ic_keyboard_black_24dp;
@@ -89,12 +101,49 @@ public class CommonTextModuleActions {
                 return true;
             }
             case ACTION_OPEN_LINK_BROWSER: {
+                //Default Methods
+//                String url;
+//                if ((url = PlainTextStuff.tryExtractUrlAroundPos(_hlEditor.getText().toString(), _hlEditor.getSelectionStart())) != null) {
+//                    if (url.endsWith(")")) {
+//                        url = url.substring(0, url.length() - 1);
+//                    }
+//                    new ContextUtils(_activity).openWebpageInExternalBrowser(url);
+//                }
+//                return true;
+
+                //NEW METHODS
                 String url;
                 if ((url = PlainTextStuff.tryExtractUrlAroundPos(_hlEditor.getText().toString(), _hlEditor.getSelectionStart())) != null) {
                     if (url.endsWith(")")) {
                         url = url.substring(0, url.length() - 1);
                     }
-                    new ContextUtils(_activity).openWebpageInExternalBrowser(url);
+                    // inflate the layout of the popup window
+                    LayoutInflater inflater = (LayoutInflater) _activity.getSystemService(LAYOUT_INFLATER_SERVICE);
+                    View popupView = inflater.inflate(R.layout.popup_window, (ViewGroup) ((ViewGroup) this._activity.findViewById(android.R.id.content)).getChildAt(0), false);
+
+                    // create the popup window
+                    int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+                    int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+                    boolean focusable = true; // lets taps outside the popup also dismiss it
+                    final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+                    // show the popup window
+                    // which view you pass in doesn't matter, it is only used for the window tolken
+                    popupWindow.showAtLocation(((ViewGroup) this._activity.findViewById(android.R.id.content)).getChildAt(0), Gravity.CENTER, 0, 0);
+                    // dismiss the popup window when touched
+                    popupView.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            popupWindow.dismiss();
+                            return true;
+                        }
+                    });
+                    ImageView imageview = popupView.findViewById(R.id.imageview_url);
+                    if (imageview != null) {
+                        new DownloadImageTask((ImageView) imageview).execute(url);
+                    } else {
+                        Toast.makeText(this._activity.getBaseContext(),"Link doesn't contains any image to show.",Toast.LENGTH_LONG).show();
+                    }
                 }
                 return true;
             }
