1026bad80e3e21bae39e149d716748c0aa14fbc4
Gregor Santner <gsantner@users.noreply.github.com>
2018-10-21 19:28:22 +0200

    Update CommonTextModuleActions.java

--- app/src/main/java/net/gsantner/markor/format/plaintext/CommonTextModuleActions.java
+++ app/src/main/java/net/gsantner/markor/format/plaintext/CommonTextModuleActions.java
@@ -143,7 +143,7 @@ public class CommonTextModuleActions {
             case ACTION_COLOR_PICKER: {
                 ColorPickerDialogBuilder
                         .with(_hlEditor.getContext())
-                        .setTitle(R.string.color_picker_title)
+                        .setTitle(R.string.color)
                         .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
                         .density(12)
                         .setOnColorSelectedListener(new OnColorSelectedListener() {
@@ -152,13 +152,13 @@ public class CommonTextModuleActions {
 //                                Toast.makeText(_hlEditor.getContext(),"onColorSelected: 0x" + Integer.toHexString(selectedColor), Toast.LENGTH_SHORT).show();
                             }
                         })
-                        .setPositiveButton(R.string.color_picker_ok, new ColorPickerClickListener() {
+                        .setPositiveButton(android.R.string.ok, new ColorPickerClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
                                 _hlEditor.setTextColor(selectedColor);
                             }
                         })
-                        .setNegativeButton(R.string.color_picker_cancel, new DialogInterface.OnClickListener() {
+                        .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                             }
