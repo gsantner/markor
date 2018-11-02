f277bc7510d91ef22e2b4fe54324f8fd4824cd54
digger <digger@blackox.org>
2018-11-02 19:01:27 +0100

    added new button to insert checkbox

--- app/src/main/java/net/gsantner/markor/format/markdown/MarkdownTextModuleActions.java
+++ app/src/main/java/net/gsantner/markor/format/markdown/MarkdownTextModuleActions.java
@@ -79,6 +79,7 @@ public class MarkdownTextModuleActions extends TextModuleActions {
             {R.string.tmaid_markdown_ul, R.drawable.ic_list_black_24dp},
             {R.string.tmaid_markdown_ol, R.drawable.ic_format_list_numbered_black_24dp},
             {R.string.tmaid_color_picker, CommonTextModuleActions.ACTION_COLOR_PICKER_ICON},
+            {R.string.tmaid_checkbox, R.drawable.checkbox},
     };
 
     private class MarkdownTextModuleActionsImpl implements View.OnClickListener, View.OnLongClickListener {
@@ -113,6 +114,10 @@ public class MarkdownTextModuleActions extends TextModuleActions {
                 }
                 case R.string.tmaid_markdown_ol: {
                     runMarkdownRegularPrefixAction("1. ");
+                    break;
+                }
+                case R.string.tmaid_checkbox: {
+                    runMarkdownRegularPrefixAction("- [ ] ");
                     break;
                 }
                 case R.string.tmaid_markdown_bold: {
