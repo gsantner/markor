1ac1e471c53e950f6caa358bb4939ea0569699c5
Gregor Santner <gsantner@mailbox.org>
2018-12-09 21:45:54 +0100

    Add Greenscale basic color scheme

--- app/src/main/java/net/gsantner/markor/activity/SettingsActivity.java
+++ app/src/main/java/net/gsantner/markor/activity/SettingsActivity.java
@@ -312,6 +312,9 @@ public class SettingsActivity extends AppCompatActivity {
                 } else if (eq(preference, R.string.pref_key__editor_basic_color_scheme_gruvbox)) {
                     _as.setEditorBasicColor(true, R.color.gruvbox_fg_dark, R.color.gruvbox_bg_dark);
                     _as.setEditorBasicColor(false, R.color.gruvbox_fg_light, R.color.gruvbox_bg_light);
+                } else if (eq(preference, R.string.pref_key__editor_basic_color_scheme_greenscale)) {
+                    _as.setEditorBasicColor(true, R.color.green_dark, R.color.black);
+                    _as.setEditorBasicColor(false, R.color.green_light, R.color.white);
                 }
 
                 if (preference.getKey().startsWith("pref_key__editor_basic_color_scheme")) {
