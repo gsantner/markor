50570d48369923821c83f7839b06fe832ec72edc
iven_k <koethke@th-brandenburg.de>
2019-08-11 19:59:24 +0200

    Removed default callback interface

--- app/src/main/java/net/gsantner/markor/activity/AudioToNoteFragment.java
+++ app/src/main/java/net/gsantner/markor/activity/AudioToNoteFragment.java
@@ -1,6 +1,5 @@
 package net.gsantner.markor.activity;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -12,21 +11,18 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
-
 import net.gsantner.markor.R;
-
 import java.io.IOException;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link AudioToNoteFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link AudioToNoteFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -37,8 +33,6 @@ public class AudioToNoteFragment extends DialogFragment {
 
     private boolean recording = false;
     private MediaRecorder recorder;
-
-    private OnFragmentInteractionListener mListener;
 
     public AudioToNoteFragment() {
         // Required empty public constructor
@@ -52,12 +46,12 @@ public class AudioToNoteFragment extends DialogFragment {
         return fragment;
     }
 
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_audio_to_note, container, false);
         Button recordingButton = view.findViewById(R.id.start_recording);
+        Log.w("DEV", "onCreateView: path: " + path);
         recordingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -88,7 +82,6 @@ public class AudioToNoteFragment extends DialogFragment {
         } catch (IOException e) {
             Log.e("DEV", "stopRecording: IOException occurred while preparing recorder", e);
         }
-
         recorder.start();
     }
 
@@ -99,43 +92,4 @@ public class AudioToNoteFragment extends DialogFragment {
     }
 
 
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
