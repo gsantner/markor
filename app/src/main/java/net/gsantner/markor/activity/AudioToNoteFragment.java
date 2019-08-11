87007617ec8764a334fd71bff35f783f70e32296
iven_k <koethke@th-brandenburg.de>
2019-08-11 18:34:45 +0200

    Created sceleton

--- app/src/main/java/net/gsantner/markor/activity/AudioToNoteFragment.java
+++ app/src/main/java/net/gsantner/markor/activity/AudioToNoteFragment.java
@@ -1,12 +1,20 @@
 package net.gsantner.markor.activity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import net.gsantner.markor.R;
 
@@ -18,15 +26,11 @@ import net.gsantner.markor.R;
  * Use the {@link AudioToNoteFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AudioToNoteFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class AudioToNoteFragment extends DialogFragment {
+    private static final String ARG_PATH = "path";
+    private String path;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private boolean recording = false;
 
     private OnFragmentInteractionListener mListener;
 
@@ -34,39 +38,31 @@ public class AudioToNoteFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AudioToNoteFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AudioToNoteFragment newInstance(String param1, String param2) {
+    public static AudioToNoteFragment newInstance(String path) {
         AudioToNoteFragment fragment = new AudioToNoteFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PATH, path);
         fragment.setArguments(args);
         return fragment;
     }
 
+
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_audio_to_note, container, false);
+        view.findViewById(R.id.start_recording).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!recording) {
+                    recording = true;
+                }
+            }
+        });
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_audio_to_note, container, false);
-    }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
