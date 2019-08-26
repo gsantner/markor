a99abedcdd8c29aad86ca6af6c7979ddd52f0226
iven_k <koethke@th-brandenburg.de>
2019-08-26 22:02:41 +0200

    Removed unnecessary imports

--- app/src/main/java/net/gsantner/markor/activity/AudioToNoteFragment.java
+++ app/src/main/java/net/gsantner/markor/activity/AudioToNoteFragment.java
@@ -7,8 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.util.LogWriter;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
