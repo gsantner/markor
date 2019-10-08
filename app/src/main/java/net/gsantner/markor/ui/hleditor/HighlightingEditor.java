1c1f6d3b8b96f1e8a4f5808393619fa6fc714f13
Carlos Olimpio <carlossolimpio@hotmail.com>
2019-10-07 21:18:36 -0300

    Update HighlightingEditor.java

--- app/src/main/java/net/gsantner/markor/ui/hleditor/HighlightingEditor.java
+++ app/src/main/java/net/gsantner/markor/ui/hleditor/HighlightingEditor.java
@@ -16,7 +16,6 @@ import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.KeyEvent;
 
 import net.gsantner.markor.activity.MainActivity;
