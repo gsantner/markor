bd27fcb35c2bbef48b6f79bc3ce3bd1321caf2bf
Gregor Santner <gsantner@mailbox.org>
2019-10-25 00:54:02 +0200

    Add prepared but missing new special keys

--- app/src/main/java/net/gsantner/markor/ui/SearchOrCustomTextDialogCreator.java
+++ app/src/main/java/net/gsantner/markor/ui/SearchOrCustomTextDialogCreator.java
@@ -63,12 +63,12 @@ public class SearchOrCustomTextDialogCreator {
             availableDataToActionMap.add(actionRes);
             availableDataToIconMap.add(iconRes);
         };
-        addToList.callback(R.string.color, R.id.action_attach_color, R.drawable.ic_image_black_24dp);
+        addToList.callback(R.string.color, R.id.action_attach_color, R.drawable.ic_format_color_fill_black_24dp);
         addToList.callback(R.string.insert_link, R.id.action_attach_link, R.drawable.ic_link_black_24dp);
         addToList.callback(R.string.file, R.id.action_attach_file, R.drawable.ic_attach_file_black_24dp);
         addToList.callback(R.string.image, R.id.action_attach_image, R.drawable.ic_image_black_24dp);
         addToList.callback(R.string.audio, R.id.action_attach_audio, R.drawable.ic_keyboard_voice_black_24dp);
-        addToList.callback(R.string.date, R.id.action_attach_date, R.drawable.ic_date_range_black_24dp);
+        addToList.callback(R.string.date, R.id.action_attach_date, R.drawable.ic_access_time_black_24dp);
 
 
         SearchOrCustomTextDialog.DialogOptions dopt = new SearchOrCustomTextDialog.DialogOptions();
