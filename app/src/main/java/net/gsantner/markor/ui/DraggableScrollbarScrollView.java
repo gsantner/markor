e225618b577a321ce8889c01116c4415603fba65
Albert-Huszár Helga Anna <h642159@stud.u-szeged.hu>
2020-05-21 14:56:07 +0200

    Draggable scrollbar in editor (#619)

---
+++ app/src/main/java/net/gsantner/markor/ui/DraggableScrollbarScrollView.java
@@ -0,0 +1,95 @@
+package net.gsantner.markor.ui;
+
+import android.widget.ScrollView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DraggableScrollbarScrollView extends ScrollView {
+
+    private boolean _isFastScrolling = false;
+    private boolean _fastScrollEnabled = true;
+    private boolean _ltr = true;
+    private int _thumbHeight;
+
+    public DraggableScrollbarScrollView(Context context) {
+        super(context);
+        setSmoothScrollingEnabled(true);
+        setLtr();
+    }
+
+    public DraggableScrollbarScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setSmoothScrollingEnabled(true);
+        setLtr();
+    }
+
+    public DraggableScrollbarScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setSmoothScrollingEnabled(true);
+        setLtr();
+    }
+
+    private void setLtr(){
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            _ltr = getLayoutDirection() == View.LAYOUT_DIRECTION_LTR;
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (!_fastScrollEnabled) {
+            return super.onInterceptTouchEvent(ev);
+        }
+        if (_isFastScrolling) {
+            return true;
+        }
+        if (ev.getActionMasked() == MotionEvent.ACTION_DOWN &&
+                (_ltr && getWidth() - getVerticalScrollbarWidth() < ev.getX()
+                        || !_ltr && getVerticalScrollbarWidth() > ev.getX())) {
+            computeThumbHeight();
+            awakenScrollBars();
+            float scrollbarStartPos = (float)computeVerticalScrollOffset() / computeVerticalScrollRange() * (getHeight());
+            if (Math.abs(ev.getY() - scrollbarStartPos) < _thumbHeight) {
+                _isFastScrolling = true;
+            }
+            return true;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        int action = ev.getActionMasked();
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            _isFastScrolling = false;
+        }
+        if (_isFastScrolling && action == MotionEvent.ACTION_MOVE) {
+            smoothScrollTo(0, (int) (((ev.getY() - _thumbHeight / 2) / (getHeight() - _thumbHeight))*(computeVerticalScrollRange() - computeVerticalScrollExtent())));
+            return true;
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    // Approximate height of thumb
+    private void computeThumbHeight() {
+        int height = (int) ((float)computeVerticalScrollExtent() * getHeight() / computeVerticalScrollRange()) ;
+        int minHeight = getHeight() / 8;
+        if (height < minHeight) {
+            height = minHeight;
+        }
+        _thumbHeight = height;
+    }
+
+    public void enableFastScroll() {
+        _fastScrollEnabled = true;
+    }
+
+    public void disableFastScroll() {
+        _fastScrollEnabled = false;
+    }
+
+
+}
