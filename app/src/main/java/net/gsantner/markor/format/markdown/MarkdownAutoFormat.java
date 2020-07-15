7d5b3850a194ff84925f097e4931456480d491f4
Harshad Srinivasan <harshad1@zoho.com>
2020-07-15 08:33:59 -0700

    Moved symbol to final var

--- app/src/main/java/net/gsantner/markor/format/markdown/MarkdownAutoFormat.java
+++ app/src/main/java/net/gsantner/markor/format/markdown/MarkdownAutoFormat.java
@@ -40,18 +40,17 @@ public class MarkdownAutoFormat implements InputFilter {
 
     @SuppressLint("DefaultLocale")
     private CharSequence autoIndent(CharSequence source, Spanned dest, int dstart, int dend) {
-        int iStart = StringUtils.getLineStart(dest, dstart);
-
-        final String result;
+        final String checkSymbol = "[ ] ";
 
         final OrderedListLine oLine = new OrderedListLine(dest, dstart);
         final UnOrderedListLine uLine = new UnOrderedListLine(dest, dstart);
         final String indent = source + StringUtils.repeatChars(' ', oLine.indent);
 
+        final String result;
         if (oLine.isOrderedList && oLine.lineEnd != oLine.groupEnd) {
             result = indent + String.format("%d%c ", oLine.value + 1, oLine.delimiter);
         } else if (uLine.isUnorderedList && uLine.lineEnd != uLine.groupEnd) {
-            final String checkString = uLine.isCheckboxList ? "[ ] " : "";
+            final String checkString = uLine.isCheckboxList ? checkSymbol : "";
             result = indent + String.format("%c %s", uLine.listChar, checkString);
         } else {
             result = indent;
