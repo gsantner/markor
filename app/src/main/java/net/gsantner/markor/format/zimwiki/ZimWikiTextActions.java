f3d14a76d50bf3b5cd5eb725367ba2b1768f639b
Frederic Jacob <frederic_jacob@web.de>
2020-10-18 20:02:26 +0200

    zimwiki(textactions): implement inline actions and add resources for actions
    now also supports bold, italic, strikethrough, marked, code inline
    also add headings with level 4 and 5

--- app/src/main/java/net/gsantner/markor/format/zimwiki/ZimWikiTextActions.java
+++ app/src/main/java/net/gsantner/markor/format/zimwiki/ZimWikiTextActions.java
@@ -12,7 +12,6 @@ package net.gsantner.markor.format.zimwiki;
 import android.app.Activity;
 import android.support.annotation.StringRes;
 import android.view.HapticFeedbackConstants;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Toast;
 
@@ -21,7 +20,6 @@ import net.gsantner.markor.format.general.CommonTextActions;
 import net.gsantner.markor.model.Document;
 import net.gsantner.markor.ui.AttachImageOrLinkDialog;
 import net.gsantner.markor.ui.SearchOrCustomTextDialogCreator;
-import net.gsantner.opoc.util.Callback;
 import net.gsantner.opoc.util.ContextUtils;
 import net.gsantner.opoc.util.StringUtils;
 
@@ -32,9 +30,8 @@ import java.util.regex.Pattern;
 
 public class ZimWikiTextActions extends net.gsantner.markor.ui.hleditor.TextActions {
 
+    // TODO: adapt these to zim wiki
     public static final Pattern PREFIX_ORDERED_LIST = Pattern.compile("^(\\s*)((\\d+)([.)])(\\s+))");
-    public static final Pattern PREFIX_ATX_HEADING = Pattern.compile("^(\\s{0,3})(#{1,6}\\s)");
-    public static final Pattern PREFIX_QUOTE = Pattern.compile("^(>\\s)");
     public static final Pattern PREFIX_CHECKED_LIST = Pattern.compile("^(\\s*)(([-*+])\\s\\[([xX])]\\s)");
     public static final Pattern PREFIX_UNCHECKED_LIST = Pattern.compile("^(\\s*)(([-*+])\\s\\[\\s]\\s)");
     public static final Pattern PREFIX_UNORDERED_LIST = Pattern.compile("^(\\s*)(([-*+])\\s)");
@@ -42,8 +39,6 @@ public class ZimWikiTextActions extends net.gsantner.markor.ui.hleditor.TextActi
 
     private static final Pattern[] PREFIX_PATTERNS = {
             PREFIX_ORDERED_LIST,
-            PREFIX_ATX_HEADING,
-            PREFIX_QUOTE,
             PREFIX_CHECKED_LIST,
             PREFIX_UNCHECKED_LIST,
             // Unordered has to be after checked list. Otherwise checklist will match as an unordered list.
@@ -61,12 +56,12 @@ public class ZimWikiTextActions extends net.gsantner.markor.ui.hleditor.TextActi
     @Override
     public boolean runAction(String action, boolean modLongClick, String anotherArg) {
         int res = new ContextUtils(_context).getResId(ContextUtils.ResType.STRING, action);
-        return new MarkdownTextActionsImpl(res).onClickImpl(null);
+        return new ZimWikiTextActionsImpl(res).onClickImpl(null);
     }
 
     @Override
     protected ActionCallback getActionCallback(@StringRes int keyId) {
-        return new MarkdownTextActionsImpl(keyId);
+        return new ZimWikiTextActionsImpl(keyId);
     }
 
     @Override
@@ -81,35 +76,35 @@ public class ZimWikiTextActions extends net.gsantner.markor.ui.hleditor.TextActi
         final ActionItem[] TMA_ACTIONS = {
                 new ActionItem(R.string.tmaid_common_checkbox_list, R.drawable.ic_check_box_black_24dp, R.string.check_list),
                 new ActionItem(R.string.tmaid_common_unordered_list_char, R.drawable.ic_list_black_24dp, R.string.unordered_list),
-                new ActionItem(R.string.tmaid_markdown_bold, R.drawable.ic_format_bold_black_24dp, R.string.bold),
-                new ActionItem(R.string.tmaid_markdown_italic, R.drawable.ic_format_italic_black_24dp, R.string.italic),
+                new ActionItem(R.string.tmaid_zimwiki_bold, R.drawable.ic_format_bold_black_24dp, R.string.bold),
+                new ActionItem(R.string.tmaid_zimwiki_strikeout, R.drawable.ic_format_strikethrough_black_24dp, R.string.strikeout),
+                new ActionItem(R.string.tmaid_zimwiki_italic, R.drawable.ic_format_italic_black_24dp, R.string.italic),
                 new ActionItem(R.string.tmaid_zimwiki_mark, R.drawable.ic_format_underlined_black_24dp, R.string.marked),
+                new ActionItem(R.string.tmaid_zimwiki_code_inline, R.drawable.ic_code_black_24dp, R.string.inline_code),
+                new ActionItem(R.string.tmaid_zimwiki_h1, R.drawable.format_header_1, R.string.heading_1),
+                new ActionItem(R.string.tmaid_zimwiki_h2, R.drawable.format_header_2, R.string.heading_2),
+                new ActionItem(R.string.tmaid_zimwiki_h3, R.drawable.format_header_3, R.string.heading_3),
+                new ActionItem(R.string.tmaid_zimwiki_h4, R.drawable.ic_info_black_24dp, R.string.heading_4),   // TODO: add h4 icon
+                new ActionItem(R.string.tmaid_zimwiki_h5, R.drawable.ic_info_black_24dp, R.string.heading_5),   // TODO: add h5 icon
                 new ActionItem(R.string.tmaid_common_delete_lines, CommonTextActions.ACTION_DELETE_LINES_ICON, R.string.delete_lines),
                 new ActionItem(R.string.tmaid_common_open_link_browser, CommonTextActions.ACTION_OPEN_LINK_BROWSER__ICON, R.string.open_link),
                 new ActionItem(R.string.tmaid_common_attach_something, R.drawable.ic_attach_file_black_24dp, R.string.attach),
                 new ActionItem(R.string.tmaid_common_special_key, CommonTextActions.ACTION_SPECIAL_KEY__ICON, R.string.special_key),
                 new ActionItem(R.string.tmaid_common_time, R.drawable.ic_access_time_black_24dp, R.string.date_and_time),
-                new ActionItem(R.string.tmaid_markdown_code_inline, R.drawable.ic_code_black_24dp, R.string.inline_code),
                 new ActionItem(R.string.tmaid_common_ordered_list_number, R.drawable.ic_format_list_numbered_black_24dp, R.string.ordered_list),
-                new ActionItem(R.string.tmaid_markdown_table_insert_columns, R.drawable.ic_view_module_black_24dp, R.string.table),
-                new ActionItem(R.string.tmaid_markdown_quote, R.drawable.ic_format_quote_black_24dp, R.string.quote),
-                new ActionItem(R.string.tmaid_markdown_h1, R.drawable.format_header_1, R.string.heading_1),
-                new ActionItem(R.string.tmaid_markdown_h2, R.drawable.format_header_2, R.string.heading_2),
-                new ActionItem(R.string.tmaid_markdown_h3, R.drawable.format_header_3, R.string.heading_3),
-                new ActionItem(R.string.tmaid_markdown_horizontal_line, R.drawable.ic_more_horiz_black_24dp, R.string.horizontal_line),
-                new ActionItem(R.string.tmaid_markdown_strikeout, R.drawable.ic_format_strikethrough_black_24dp, R.string.strikeout),
                 new ActionItem(R.string.tmaid_common_accordion, R.drawable.ic_arrow_drop_down_black_24dp, R.string.accordion),
                 new ActionItem(R.string.tmaid_common_indent, R.drawable.ic_format_indent_increase_black_24dp, R.string.indent),
                 new ActionItem(R.string.tmaid_common_deindent, R.drawable.ic_format_indent_decrease_black_24dp, R.string.deindent),
+                // TODO: insert link, insert image
         };
 
         return Arrays.asList(TMA_ACTIONS);
     }
 
-    private class MarkdownTextActionsImpl extends ActionCallback {
+    private class ZimWikiTextActionsImpl extends ActionCallback {
         private int _action;
 
-        MarkdownTextActionsImpl(int action) {
+        ZimWikiTextActionsImpl(int action) {
             _action = action;
         }
 
@@ -123,29 +118,35 @@ public class ZimWikiTextActions extends net.gsantner.markor.ui.hleditor.TextActi
                 view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP);
             }
             switch (_action) {
-                case R.string.tmaid_markdown_quote: {
-                    runPrefixReplaceAction(PREFIX_QUOTE, ">$1 ", "");
-                    return true;
-                }
-                case R.string.tmaid_markdown_h1: {
+                case R.string.tmaid_zimwiki_h1: {
                     setHeadingAction(1);
                     return true;
                 }
-                case R.string.tmaid_markdown_h2: {
+                case R.string.tmaid_zimwiki_h2: {
                     setHeadingAction(2);
                     return true;
                 }
-                case R.string.tmaid_markdown_h3: {
+                case R.string.tmaid_zimwiki_h3: {
                     setHeadingAction(3);
                     return true;
                 }
+                case R.string.tmaid_zimwiki_h4: {
+                    setHeadingAction(4);
+                    return true;
+                }
+                case R.string.tmaid_zimwiki_h5: {
+                    setHeadingAction(5);
+                    return true;
+                }
                 case R.string.tmaid_common_unordered_list_char: {
+                    // TODO: adapt to zim wiki
                     final String listChar = _appSettings.getUnorderedListCharacter();
                     final String listPrefix = "$1" + listChar + " ";
                     runPrefixReplaceAction(PREFIX_UNORDERED_LIST, listPrefix, "$1");
                     return true;
                 }
                 case R.string.tmaid_common_checkbox_list: {
+                    // TODO: adapt to zim wiki
                     final String listChar = _appSettings.getUnorderedListCharacter();
                     final String uncheck = "$1" + listChar + " [ ] ";
                     final String check = "$1" + listChar + " [x] ";
@@ -153,40 +154,43 @@ public class ZimWikiTextActions extends net.gsantner.markor.ui.hleditor.TextActi
                     return true;
                 }
                 case R.string.tmaid_common_ordered_list_number: {
+                    // TODO: adapt to zim wiki
                     runPrefixReplaceAction(PREFIX_ORDERED_LIST, "$11. ", "$1");
                     runRenumberOrderedListIfRequired();
                     return true;
                 }
-                case R.string.tmaid_markdown_bold: {
+                case R.string.tmaid_zimwiki_bold: {
                     runInlineAction("**");
                     return true;
                 }
-                case R.string.tmaid_markdown_italic: {
-                    runInlineAction("_");
+                case R.string.tmaid_zimwiki_italic: {
+                    runInlineAction("//");
                     return true;
                 }
-                case R.string.tmaid_markdown_strikeout: {
+                case R.string.tmaid_zimwiki_mark: {
+                    runInlineAction("__");
+                    return true;
+                }
+                case R.string.tmaid_zimwiki_strikeout: {
                     runInlineAction("~~");
                     return true;
                 }
-                case R.string.tmaid_markdown_code_inline: {
-                    runInlineAction("`");
+                case R.string.tmaid_zimwiki_code_inline: {
+                    runInlineAction("''");
                     return true;
                 }
-                case R.string.tmaid_markdown_horizontal_line: {
-                    runInlineAction("----\n");
-                    return true;
-                }
-                case R.string.tmaid_markdown_table_insert_columns: {
-                    SearchOrCustomTextDialogCreator.showInsertTableRowDialog(_activity, false, callbackInsertTableRow);
-                    return true;
-                }
-                case R.string.tmaid_markdown_insert_link:
-                case R.string.tmaid_markdown_insert_image: {
-                    AttachImageOrLinkDialog.showInsertImageOrLinkDialog(_action == R.string.tmaid_markdown_insert_image ? 2 : 3, _document.getFormat(), _activity, _hlEditor, _document.getFile());
+//                case R.string.tmaid_zimwiki_horizontal_line: {
+//                    runInlineAction("----\n");
+//                    return true;
+//                }
+                case R.string.tmaid_zimwiki_insert_link:
+                case R.string.tmaid_zimwiki_insert_image: {
+                    // TODO: adapt to zim wiki
+                    AttachImageOrLinkDialog.showInsertImageOrLinkDialog(_action == R.string.tmaid_zimwiki_insert_image ? 2 : 3, _document.getFormat(), _activity, _hlEditor, _document.getFile());
                     return true;
                 }
                 case R.string.tmaid_common_toolbar_title_clicked_edit_action: {
+                    // TODO: adapt to zim wiki
                     final String origText = _hlEditor.getText().toString();
                     SearchOrCustomTextDialogCreator.showMarkdownHeadlineDialog(_activity, origText, callbackPayload -> {
                         int cursor = origText.indexOf(callbackPayload);
@@ -217,7 +221,8 @@ public class ZimWikiTextActions extends net.gsantner.markor.ui.hleditor.TextActi
                     new CommonTextActions(_activity, _hlEditor).runAction(CommonTextActions.ACTION_JUMP_BOTTOM_TOP);
                     return true;
                 }
-                case R.string.tmaid_markdown_insert_image: {
+                case R.string.tmaid_zimwiki_insert_image: {
+                    // TODO: adapt to zim wiki
                     int pos = _hlEditor.getSelectionStart();
                     _hlEditor.getText().insert(pos, "<img style=\"width:auto;max-height: 256px;\" src=\"\" />");
                     _hlEditor.setSelection(pos + 48);
@@ -227,11 +232,8 @@ public class ZimWikiTextActions extends net.gsantner.markor.ui.hleditor.TextActi
                     runCommonTextAction("tmaid_common_time_insert_timestamp");
                     return true;
                 }
-                case R.string.tmaid_markdown_table_insert_columns: {
-                    SearchOrCustomTextDialogCreator.showInsertTableRowDialog(_activity, true, callbackInsertTableRow);
-                    break;
-                }
-                case R.string.tmaid_markdown_code_inline: {
+                case R.string.tmaid_zimwiki_code_inline: {
+                    // TODO: adapt to zim wiki
                     _hlEditor.disableHighlighterAutoFormat();
                     final int c = _hlEditor.setSelectionExpandWholeLines();
                     _hlEditor.getText().insert(_hlEditor.getSelectionStart(), "\n```\n");
@@ -242,37 +244,12 @@ public class ZimWikiTextActions extends net.gsantner.markor.ui.hleditor.TextActi
                     return true;
                 }
                 case R.string.tmaid_common_ordered_list_number: {
+                    // TODO: adapt to zim wiki
                     ZimWikiAutoFormat.renumberOrderedList(_hlEditor.getText(), StringUtils.getSelection(_hlEditor)[0]);
                 }
             }
             return false;
         }
-
-        private final Callback.a2<Integer, Boolean> callbackInsertTableRow = (cols, isHeaderEnabled) -> {
-            StringBuilder sb = new StringBuilder();
-            _hlEditor.requestFocus();
-            if (!_hlEditor.isCurrentLineEmpty()) {
-                sb.append("\n");
-            }
-            for (int i = 0; i < cols - 1; i++) {
-                sb.append("  | ");
-            }
-            if (isHeaderEnabled) {
-                sb.append("\n");
-                for (int i = 0; i < cols; i++) {
-                    sb.append("---");
-                    if (i < cols - 1) {
-                        sb.append("|");
-                    }
-                }
-            }
-            _hlEditor.moveCursorToEndOfLine(0);
-            _hlEditor.insertOrReplaceTextOnCursor(sb.toString());
-            _hlEditor.moveCursorToBeginOfLine(0);
-            if (isHeaderEnabled) {
-                _hlEditor.simulateKeyPress(KeyEvent.KEYCODE_DPAD_UP);
-            }
-        };
     }
 
 
