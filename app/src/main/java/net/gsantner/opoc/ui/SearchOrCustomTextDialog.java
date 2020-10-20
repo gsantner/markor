9c7ee86055990e54710d32c883af761a9a21ee0d
Frederic Jacob <frederic_jacob@web.de>
2020-10-20 15:32:31 +0200

    fix crashing file search dialog caused by null object
    fixes TargetInvocationException when querying dopt.data

--- app/src/main/java/net/gsantner/opoc/ui/SearchOrCustomTextDialog.java
+++ app/src/main/java/net/gsantner/opoc/ui/SearchOrCustomTextDialog.java
@@ -65,7 +65,7 @@ public class SearchOrCustomTextDialog {
     public static class DialogOptions {
         public Callback.a1<String> callback;
         public Callback.a2<String, Integer> withPositionCallback;
-        public List<? extends CharSequence> data;
+        public List<? extends CharSequence> data = new ArrayList<>();
         public List<? extends CharSequence> highlightData;
         public List<Integer> iconsForData;
         public String messageText = "";
@@ -187,7 +187,6 @@ public class SearchOrCustomTextDialog {
     }
 
     public static void showMultiChoiceDialogWithSearchFilterUI(final Activity activity, final DialogOptions dopt) {
-        final List<CharSequence> allItems = new ArrayList<>(dopt.data);
         final List<Pair<String, Integer>> filteredItems = new ArrayList<>();
         final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity, dopt.isDarkDialog
                 ? android.support.v7.appcompat.R.style.Theme_AppCompat_Dialog
