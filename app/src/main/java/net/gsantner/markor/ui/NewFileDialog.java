2283b255baa8e217433cb783272a1380efe75ae7
Gregor Santner <gsantner@mailbox.org>
2021-03-24 01:15:12 +0100

    remove unneeded lines

--- app/src/main/java/net/gsantner/markor/ui/NewFileDialog.java
+++ app/src/main/java/net/gsantner/markor/ui/NewFileDialog.java
@@ -114,13 +114,9 @@ public class NewFileDialog extends DialogFragment {
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
 
-            ;
-
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
             }
-
-            ;
 
             @Override
             public void afterTextChanged(Editable s) {
