958917b0734b1b0a8e1df77322109ab107af1ff0
Florian Tham <fgt@mailbox.org>
2022-02-04 13:55:57 +0100

    Don't modify math env in text converter

--- app/src/main/java/net/gsantner/markor/format/markdown/MarkdownTextConverter.java
+++ app/src/main/java/net/gsantner/markor/format/markdown/MarkdownTextConverter.java
@@ -194,7 +194,6 @@ public class MarkdownTextConverter extends TextConverter {
         if (appSettings.isMarkdownMathEnabled() && markup.contains("$")) {
             head += HTML_KATEX_INCLUDE;
             onLoadJs += JS_KATEX;
-            markup = markup.replaceAll("(?ms)^([$]{2}.*?[$]{2})$", "<div>\n$1\n</div>");
         }
 
         // Enable View (block) code syntax highlighting
