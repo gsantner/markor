10e0bcda47391d98c6bba77355be4342b715b7eb
Florian Tham <fgt@mailbox.org>
2022-02-10 21:55:53 +0100

    Add licensing info and brief description

--- app/thirdparty/java/other/com/vladsch/flexmark/ext/katex/FlexmarkKatexExtension.java
+++ app/thirdparty/java/other/com/vladsch/flexmark/ext/katex/FlexmarkKatexExtension.java
@@ -1,4 +1,34 @@
-// TODO: Add license notice (=derivative from gitlab extension)
+/*##############################################################################
+ *
+ * Copyright (c) 2015-2016, Atlassian Pty Ltd
+ * All rights reserved.
+ *
+ * Copyright (c) 2016-2018, Vladimir Schneider,
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *#############################################################################*/
+
 package other.com.vladsch.flexmark.ext.katex;
 
 import com.vladsch.flexmark.ast.DelimitedNode;
@@ -30,8 +60,16 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-// TODO: Add bit of description about origin (for maintainence)
-// forked from gitlab extension, link the files https:// blabla
+/* This extension is based on Flexmark's GitLab Flavoured Markdown Extension[1].
+ * The sources [2] were stripped of everything not relevant to math parsing.
+ *
+ * The extension implements parsers for inline math mode, `$...$`, and for display
+ * math mode, `$$...$$`. Both are based on `internal/GitLabInlineMathParser.java`,
+ * i.e. both are InlineParsers, not BlockParsers.
+ *
+ * [1]: https://github.com/vsch/flexmark-java/wiki/Extensions#gitlab-flavoured-markdown
+ * [2]: https://github.com/vsch/flexmark-java/tree/0.42.14/flexmark-ext-gitlab/src/main/java/com/vladsch/flexmark/ext/gitlab
+ */
 public class FlexmarkKatexExtension {
 
     /**
