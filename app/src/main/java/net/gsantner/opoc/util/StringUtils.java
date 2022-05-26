c9db5c6b521821ca98c2417d4e7bd44f25523b20
Harshad Vedartham <harshad1@users.noreply.github.com>
2022-05-26 15:35:27 -0700

    Don't need to sync
    Both calls are internally synced anyway

--- app/src/main/java/net/gsantner/opoc/util/StringUtils.java
+++ app/src/main/java/net/gsantner/opoc/util/StringUtils.java
@@ -700,12 +700,9 @@ public final class StringUtils {
     // Debounce any callback
     public static Runnable makeDebounced(final Handler handler, final long delayMs, final Runnable callback) {
         final Handler _handler = handler == null ? new Handler(Looper.getMainLooper()) : handler;
-        final Object sync = new Object();
         return () -> {
-            synchronized (sync) {
-                _handler.removeCallbacks(callback);
-                _handler.postDelayed(callback, delayMs);
-            }
+            _handler.removeCallbacks(callback);
+            _handler.postDelayed(callback, delayMs);
         };
     }
 }
