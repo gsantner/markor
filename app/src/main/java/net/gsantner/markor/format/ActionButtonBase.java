706cdb19334a4c104f61b77a561de22e4440c3e8
Harshad Vedartham <harshad1@zoho.com>
2022-08-31 08:27:36 -0700

    Cleanup

--- app/src/main/java/net/gsantner/markor/format/ActionButtonBase.java
+++ app/src/main/java/net/gsantner/markor/format/ActionButtonBase.java
@@ -58,10 +58,8 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 
 @SuppressWarnings({"WeakerAccess", "UnusedReturnValue"})
 public abstract class ActionButtonBase {
