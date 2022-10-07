70c21c331794cf3aaebd24ccdb14033715f925ad
Germo Görtz <github.com@aisberg.de>
2022-10-07 13:15:22 +0200

    formatting for some delimited blocks

--- app/src/main/java/net/gsantner/markor/format/asciidoc/AsciidocSyntaxHighlighter.java
+++ app/src/main/java/net/gsantner/markor/format/asciidoc/AsciidocSyntaxHighlighter.java
@@ -28,45 +28,50 @@ public class AsciidocSyntaxHighlighter extends SyntaxHighlighterBase {
     // bold formatting pair).
     // Italic syntax (_) is always the innermost formatting pair.
 
-    // TODO: simplified, OK for basic examples
+    // simplified, OK for basic examples
     public final static Pattern BOLD = Pattern
             .compile("(?m)(\\*(?!\\*)\\b(.*?)\\b\\*(?!\\*))|(\\*(?!\\*)_(.*?)_\\*(?!\\*))|(\\*\\*(?!\\*)(.*?)\\*\\*)");
-    // TODO: simplified, OK for basic examples
+    // simplified, OK for basic examples
     public final static Pattern ITALICS = Pattern.compile("(?m)(\\b_(?!_)(.*?)_(?!_)\\b)|(__(?!_)(.*?)__)");
-    // TODO: simplified, OK for basic examples
+    // simplified, OK for basic examples, contains only inline code
     public final static Pattern CODE = Pattern
             .compile("(?m)(`(?!`)\\b(.*?)`(?!`))|(`(?!`)_(.*?)_`(?!`))|(`(?!`)\\*(.*?)\\*`(?!`))|(``(?!`)(.*?)``)");
-    // public final static Pattern HEADING =
-    // Pattern.compile("(?m)((^={1,6}[^\\S\\n][^\\n]+)|((\\n|^)[^\\s]+.*?\\n(-{2,}|={2,})[^\\S\\n]*$))");
-    // DONE
     public final static Pattern HEADING_SIMPLE = Pattern.compile("(?m)^(={1,6} {1}\\S.*$)");
-    // DONE
     public final static Pattern LIST_UNORDERED = Pattern.compile("(?m)^(\\*{1,6})( {1})\\S.*$");
-    // DONE
     public final static Pattern LIST_ORDERED = Pattern.compile("(?m)^(\\.{1,6})( {1})\\S.*$");
-    // TODO: simplified, OK for basic examples
+    // simplified, OK for basic examples
     public final static Pattern HIGHLIGHT = Pattern.compile("(?m)(#(?!#)(.*?)#(?!#))|(##(?!#)(.*?)##)");
-    // TODO: simplified, OK for basic examples
+    // simplified, OK for basic examples
     public final static Pattern STRIKETHROUGH = Pattern
             .compile("(?m)\\[\\.line-through\\]((#(?!#)(.*?)#(?!#))|(##(?!#)(.*?)##))");
+    public final static Pattern HARD_LINE_BREAK = Pattern.compile("(?m)(?<=\\S)([^\\S\\n]{1})\\+\\n");
 
-    // TODO: simplified, OK for basic examples
+    // simplified, OK for basic examples
     public final static Pattern LINK = Pattern.compile("link:\\S*?\\[([^\\[]*)\\]");
-    // TODO: simplified, OK for basic examples
-    public final static Pattern ACTION_IMAGE_PATTERN = Pattern.compile("\"image:\\\\S*?\\\\[([^\\\\[]*)\\\\]");
-    // unchanged
-    public final static Pattern DOUBLESPACE_LINE_ENDING = Pattern.compile("(?m)(?<=\\S)([^\\S\\n]{2,})\\n");
-    // TODO: still markdown, block should be used
-    public final static Pattern QUOTATION = Pattern.compile("(\\n|^)>");
-    // TODO: still markdown
-    public final static Pattern ACTION_LINK_PATTERN = Pattern.compile("(?m)\\[(.*?)\\]\\((.*?)\\)");
-    // TODO: block syntax
+    public final static Pattern XREF = Pattern.compile("xref:\\S*?\\[([^\\[]*)\\]");
+    public final static Pattern ACTION_IMAGE_PATTERN = Pattern.compile("image:\\S*?\\[([^\\[]*)\\]");
+    // TODO: include
 
+    // block syntax
+    // simplified, contains only the most common case, like "____", "....", "----", ...
+    public final static Pattern BLOCK_DELIMITED_QUOTATION = Pattern.compile("(?m)^\\_{4}[\\r\\n]([\\s\\S]+?(?=^\\_{4}[\\r\\n]))\\_{4}[\\r\\n]");
+    public final static Pattern BLOCK_DELIMITED_LISTING = Pattern.compile("(?m)^\\-{4}[\\r\\n]([\\s\\S]+?(?=^\\-{4}[\\r\\n]))\\-{4}[\\r\\n]");
+    public final static Pattern BLOCK_DELIMITED_LITERAL = Pattern.compile("(?m)^\\.{4}[\\r\\n]([\\s\\S]+?(?=^\\.{4}[\\r\\n]))\\.{4}[\\r\\n]");
+//
+//    // unchanged
+////    public final static Pattern DOUBLESPACE_LINE_ENDING = Pattern.compile("(?m)(?<=\\S)([^\\S\\n]{2,})\\n");
+//
+//    // TODO: still markdown, but it is not used
+//    public final static Pattern ACTION_LINK_PATTERN = Pattern.compile("(?m)\\[(.*?)\\]\\((.*?)\\)");
+//
     // TODO better colors for red green color weakness
     // or use settings and predefined color sets for "normal" users and handicapped
     // users
-    private static final int AD_COLOR_HEADING = 0xba3925ff;
-    private static final int AD_COLOR_LINK = 0x2156a5ff;
+    // 0xba3925ff;
+    // 0x2156a5ff;
+
+    private static final int AD_COLOR_HEADING = 0xffef6D00;
+    private static final int AD_COLOR_LINK = 0xff1ea3fe;
     private static final int AD_COLOR_LIST = 0xffdaa521;
     private static final int AD_COLOR_QUOTE = 0xff88b04c;
     private static final int AD_COLOR_CODEBLOCK = 0xff8c8c8c;
@@ -103,32 +108,37 @@ public class AsciidocSyntaxHighlighter extends SyntaxHighlighterBase {
         // but not in the current Pull Request
         createSmallBlueLinkSpans();
 
-        // TODO: add little by little
         if (_highlightBiggerHeadings) {
             createSpanForMatches(HEADING_SIMPLE,
                     new WrMarkdownHeaderSpanCreator(_spannable, AD_COLOR_HEADING, _textSize));
         } else {
             createColorSpanForMatches(HEADING_SIMPLE, AD_COLOR_HEADING);
         }
+
         createColorSpanForMatches(LINK, AD_COLOR_LINK);
+        createColorSpanForMatches(XREF, AD_COLOR_LINK);
         createColorSpanForMatches(LIST_UNORDERED, AD_COLOR_LIST);
         createColorSpanForMatches(LIST_ORDERED, AD_COLOR_LIST);
 
         if (_highlightLineEnding) {
-            createColorBackgroundSpan(DOUBLESPACE_LINE_ENDING, AD_COLOR_CODEBLOCK);
+            createColorBackgroundSpan(HARD_LINE_BREAK, AD_COLOR_CODEBLOCK);
         }
 
         createStyleSpanForMatches(BOLD, Typeface.BOLD);
         createStyleSpanForMatches(ITALICS, Typeface.ITALIC);
-        createColorSpanForMatches(QUOTATION, AD_COLOR_QUOTE);
+        createColorSpanForMatches(BLOCK_DELIMITED_QUOTATION, AD_COLOR_QUOTE);
         createStrikeThroughSpanForMatches(STRIKETHROUGH);
 
         if (_highlightCodeChangeFont) {
             createMonospaceSpanForMatches(CODE);
+            createMonospaceSpanForMatches(BLOCK_DELIMITED_LISTING);
+            createMonospaceSpanForMatches(BLOCK_DELIMITED_LITERAL);
         }
 
         if (_highlightCodeBlock) {
             createColorBackgroundSpan(CODE, AD_COLOR_CODEBLOCK);
+            createColorBackgroundSpan(BLOCK_DELIMITED_LISTING, AD_COLOR_CODEBLOCK);
+            createColorBackgroundSpan(BLOCK_DELIMITED_LITERAL, AD_COLOR_CODEBLOCK);
         }
     }
 
