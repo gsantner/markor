feb64f6b2286c69b7c73ddbe86af95c7c3da3b5c
k3b <1374583+k3b@users.noreply.github.com>
2023-02-19 15:42:13 +0100

    #1980: csv is no more linked to KeyValueTextConverter

--- app/src/main/java/net/gsantner/markor/format/keyvalue/KeyValueTextConverter.java
+++ app/src/main/java/net/gsantner/markor/format/keyvalue/KeyValueTextConverter.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @SuppressWarnings("WeakerAccess")
 public class KeyValueTextConverter extends PlaintextTextConverter {
-    private static final List<String> EXT = Arrays.asList(".yml", ".yaml", ".toml", ".vcf", ".ics", ".ini", ".json", ".csv", ".zim");
+    private static final List<String> EXT = Arrays.asList(".yml", ".yaml", ".toml", ".vcf", ".ics", ".ini", ".json", ".zim");
 
     @Override
     public boolean isFileOutOfThisFormat(String filepath, String extWithDot) {
