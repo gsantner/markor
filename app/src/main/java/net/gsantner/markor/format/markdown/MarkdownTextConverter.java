e398ac28745cbd49908eb0f8bc00b383b0b85a22
Li Guanglin <2311987902@qq.com>
2023-10-07 09:03:21 +0800

    refactor(display): increase the padding(HTML body) to 2vw

--- app/src/main/java/net/gsantner/markor/format/markdown/MarkdownTextConverter.java
+++ app/src/main/java/net/gsantner/markor/format/markdown/MarkdownTextConverter.java
@@ -83,7 +83,7 @@ public class MarkdownTextConverter extends TextConverterBase {
     //########################
     //## Injected CSS / JS / HTML
     //########################
-    public static final String CSS_BODY = CSS_S + "body{margin:0;padding:1vw}" + CSS_E;
+    public static final String CSS_BODY = CSS_S + "body{margin:0;padding:2vw}" + CSS_E;
     public static final String CSS_HEADER_UNDERLINE = CSS_S + " .header_no_underline { text-decoration: none; color: " + TOKEN_BW_INVERSE_OF_THEME + "; } h1 < a.header_no_underline { border-bottom: 2px solid #eaecef; } " + CSS_E;
     public static final String CSS_H1_H2_UNDERLINE = CSS_S + " h1,h2 { border-bottom: 2px solid " + TOKEN_BW_INVERSE_OF_THEME_HEADER_UNDERLINE + "; } " + CSS_E;
     public static final String CSS_BLOCKQUOTE_VERTICAL_LINE = CSS_S + "blockquote{padding:0px 14px;border-" + TOKEN_TEXT_DIRECTION + ":3.5px solid #dddddd;margin:4px 0}" + CSS_E;
