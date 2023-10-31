63fe2b99c5a94f83ac910374c45172a660cdd477
Harshad Vedartham <harshad1@zoho.com>
2023-10-30 21:32:48 -0700

    Cleaned up imports

--- app/src/main/java/net/gsantner/markor/frontend/NewFileDialog.java
+++ app/src/main/java/net/gsantner/markor/frontend/NewFileDialog.java
@@ -15,7 +15,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.text.InputFilter;
 import android.text.TextUtils;
-import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
@@ -371,7 +370,7 @@ public class NewFileDialog extends DialogFragment {
             if (sel instanceof String && snippets.containsKey((String) sel)) {
                 final String t = GsFileUtils.readTextFileFast(snippets.get((String) sel)).first;
                 final String title = GsFileUtils.getNameWithoutExtension(filename);
-                TextViewUtils.interpolatePlaceholders(t, title, processAndCallback);
+                TextViewUtils.interpolateSnippet(t, title, "", processAndCallback);
             } else {
                 processAndCallback.callback(null);
             }
