c096b4877129f9d2d2ff98a1685b4fbb24be2df6
Li Guanglin <2311987902@qq.com>
2023-11-11 10:10:15 +0800

    refactor(code blocks): improve word wrap

--- app/thirdparty/assets/prism/main.js
+++ app/thirdparty/assets/prism/main.js
@@ -1,13 +1,7 @@
 function wrapCodeBlockWords() {
     let preElements = document.querySelectorAll("pre[class*='language-']");
     preElements.forEach((element) => {
-        element.style.wordWrap = "break-word";
         element.style.whiteSpace = "pre-wrap";
-    });
-
-    let codeElements = document.querySelectorAll("pre[class*='language-'] > code");
-    codeElements.forEach((element) => {
-        element.style.wordWrap = "break-word";
-        element.style.whiteSpace = "pre-wrap";
+        element.style.overflowWrap = "break-word";
     });
 }
