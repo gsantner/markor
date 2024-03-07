da85c371fbaa5a67aeec616db53cd3ec9a6e4f86
Tim <timreset@gmail.com>
2024-03-07 19:39:29 +0100

    issue 2232 Incorrect URL highlighting. Test.

--- app/src/test/java/other/writeily/UrlPatternTest.java
+++ app/src/test/java/other/writeily/UrlPatternTest.java
@@ -14,6 +14,8 @@ public class UrlPatternTest {
         check("begin http://test/test2 end", "http://test/test2");
         check("begin https://test/test2 end", "https://test/test2");
         check("begin https://www.test/test2 end", "https://www.test/test2");
+        check("[foo](http://bar.baz )", "http://bar.baz");
+        check("[foo](http://bar.baz)", "http://bar.baz");
 
         check("begin /test/test2 end");
         check("begin ftp://test/test2 end");
