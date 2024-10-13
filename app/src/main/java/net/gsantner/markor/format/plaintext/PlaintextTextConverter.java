a88213becd4e8e8a64371e1c3979957690c8b083
Gregor Santner <gsantner@mailbox.org>
2024-10-13 23:02:03 +0200

    Open .lrc extension as plaintext by default, closes #2322

--- app/src/main/java/net/gsantner/markor/format/plaintext/PlaintextTextConverter.java
+++ app/src/main/java/net/gsantner/markor/format/plaintext/PlaintextTextConverter.java
@@ -30,7 +30,7 @@ public class PlaintextTextConverter extends TextConverterBase {
     private static final String HTML101_BODY_PRE_END = "</pre>";
     private static final List<String> EXT_TEXT = Arrays.asList(".txt", ".taskpaper", ".org", ".ldg", ".ledger", ".m3u", ".m3u8");
     private static final List<String> EXT_HTML = Arrays.asList(".html", ".htm");
-    private static final List<String> EXT_CODE_HL = Arrays.asList(".py", ".cpp", ".h", ".c", ".js", ".mjs", ".css", ".cs", ".kt", ".lua", ".perl", ".java", ".qml", ".diff", ".php", ".r", ".patch", ".rs", ".swift", ".ts", ".mm", ".go", ".sh", ".rb", ".tex", ".xml", ".xlf");
+    private static final List<String> EXT_CODE_HL = Arrays.asList(".py", ".cpp", ".h", ".c", ".js", ".mjs", ".css", ".cs", ".kt", ".lua", ".lrc", ".perl", ".java", ".qml", ".diff", ".php", ".r", ".patch", ".rs", ".swift", ".ts", ".mm", ".go", ".sh", ".rb", ".tex", ".xml", ".xlf");
     private static final List<String> EXT = new ArrayList<>();
 
     static {
