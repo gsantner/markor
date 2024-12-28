6dc69162a2d38043e0bb41242acd6b787bf1e8c4
Harshad Vedartham <harshad1@zoho.com>
2024-12-28 23:55:33 +0530

    Cleanup

--- app/thirdparty/java/other/writeily/format/WrProportionalHeaderSpanCreator.java
+++ app/thirdparty/java/other/writeily/format/WrProportionalHeaderSpanCreator.java
@@ -38,8 +38,6 @@ public class WrProportionalHeaderSpanCreator {
 
         @Override
         public void updateMeasureState(@NonNull TextPaint textPaint) {
-            textPaint.setFakeBoldText(true);
-            textPaint.setColor(_color);
             textPaint.setTextSize(textPaint.getTextSize() * _proportion);
         }
 
@@ -47,7 +45,6 @@ public class WrProportionalHeaderSpanCreator {
         public void updateDrawState(@NonNull TextPaint textPaint) {
             textPaint.setFakeBoldText(true);
             textPaint.setColor(_color);
-            textPaint.setTextSize(textPaint.getTextSize() * _proportion);
         }
     }
 }
