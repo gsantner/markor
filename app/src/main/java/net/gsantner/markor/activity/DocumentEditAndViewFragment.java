gsantner/markor

commit 64ff85b6925224d9a0a3426f46ee8fdc0ff1d1c8

Better layout

app/src/main/java/net/gsantner/markor/activity/DocumentEditAndViewFragment.java
@@ -33,6 +33,7 @@ import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -268,9 +269,6 @@ public class DocumentEditAndViewFragment extends MarkorBaseFragment implements F
 
         _hlEditor.recomputeHighlighting(); // Run before setting scroll position
         TextViewUtils.setSelectionAndShow(_hlEditor, startPos);
-
-        _editorHolder.invalidate();
-        _editorHolder.post(() -> _hlEditor.setMinHeight(_editorHolder.getHeight()));
     }
 
     @Override
@@ -694,14 +692,13 @@ public class DocumentEditAndViewFragment extends MarkorBaseFragment implements F
         if (activity != null) {
             final View bar = activity.findViewById(R.id.document__fragment__edit__text_actions_bar);
             final View parent = activity.findViewById(R.id.document__fragment__edit__text_actions_bar__scrolling_parent);
-            final View editScroll = activity.findViewById(R.id.document__fragment__edit__content_editor__scrolling_parent);
             final View viewScroll = activity.findViewById(R.id.document__fragment_view_webview);
 
-            if (bar != null && parent != null && editScroll != null && viewScroll != null) {
+            if (bar != null && parent != null && _verticalScrollView != null && viewScroll != null) {
                 final boolean hide = _textActionsBar.getChildCount() == 0;
                 parent.setVisibility(hide ? View.GONE : View.VISIBLE);
                 final int marginBottom = hide ? 0 : (int) getResources().getDimension(R.dimen.textactions_bar_height);
-                setMarginBottom(editScroll, marginBottom);
+                setMarginBottom(_verticalScrollView, marginBottom);
                 setMarginBottom(viewScroll, marginBottom);
             }
         }
@@ -745,31 +742,35 @@ public class DocumentEditAndViewFragment extends MarkorBaseFragment implements F
         return _horizontalScrollView == null || (_hlEditor.getParent() != _horizontalScrollView);
     }
 
-    private void makeHorizontalScrollView() {
-        if (_horizontalScrollView != null) {
-            return;
-        }
-
-        _horizontalScrollView = new HorizontalScrollView(getContext());
-        _horizontalScrollView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        _horizontalScrollView.setFillViewport(true);
-    }
-
     private void setWrapState(final boolean wrap) {
         final Context context = getContext();
         if (context != null && _hlEditor != null && isWrapped() != wrap) {
             final int[] sel = TextViewUtils.getSelection(_hlEditor);
             final boolean hlEnabled = _hlEditor.setHighlightingEnabled(false);
 
-            makeHorizontalScrollView();
+            if (_horizontalScrollView == null) {
+                _horizontalScrollView = new HorizontalScrollView(getContext());
+                _horizontalScrollView.setFillViewport(true);
+            }
+
+            // Layout for child of LinearLayout
+            final LinearLayout.LayoutParams holderChildLayout = new LinearLayout.LayoutParams(
+                    0, ViewGroup.LayoutParams.WRAP_CONTENT);
+            holderChildLayout.weight = 1;
 
             if (wrap) {
                 _horizontalScrollView.removeView(_hlEditor);
                 _editorHolder.removeView(_horizontalScrollView);
+                _hlEditor.setLayoutParams(holderChildLayout);
                 _editorHolder.addView(_hlEditor, 1);
             } else {
                 _editorHolder.removeView(_hlEditor);
+                // Match_parent for child and wrap_content for parent is supported
+                // https://stackoverflow.com/a/4607808
+                _hlEditor.setLayoutParams(new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
                 _horizontalScrollView.addView(_hlEditor);
+                _horizontalScrollView.setLayoutParams(holderChildLayout);
                 _editorHolder.addView(_horizontalScrollView, 1);
             }
 

app/src/main/res/layout/document__fragment__edit.xml
@@ -37,16 +37,21 @@
                 android:layout_weight="0"
                 android:text="" />
 
+            <!--
+                Match_parent for child and wrap_content for parent is supported
+                https://stackoverflow.com/a/4607808
+            -->
             <net.gsantner.markor.frontend.textview.HighlightingEditor
                 android:id="@+id/document__fragment__edit__highlighting_editor"
-                android:layout_width="match_parent"
-                android:layout_height="wrap_content"
+                android:layout_width="0dp"
+                android:layout_height="match_parent"
                 android:layout_weight="1"
                 android:background="@color/background"
                 android:gravity="top"
                 android:imeOptions="flagNoExtractUi"
                 android:inputType="textMultiLine|textCapSentences"
                 android:minHeight="1dp"
+                android:minWidth="1dp"
                 android:paddingLeft="@dimen/activity_horizontal_margin"
                 android:paddingTop="@dimen/activity_vertical_margin"
                 android:paddingRight="@dimen/activity_horizontal_margin"
@@ -55,6 +60,7 @@
                 android:overScrollMode="never"
                 android:textCursorDrawable="@drawable/cursor_accent" />
         </LinearLayout>
+
     </net.gsantner.markor.frontend.DraggableScrollbarScrollView>
 
     <net.gsantner.opoc.frontend.GsHorizontalScrollView

commit e7dc78bee46f85f240d9b185dcc68fd2143afaad

fix: stabilize editor layout sizing to prevent large-file open slowdown (PR #2769, closes #2739 #2652 #2716)
* fix: stabilize editor layout sizing to prevent large-file open slowdown

Restore one-shot min-height initialization and avoid per-layout min-height churn, with guardrail comments to prevent reintroducing startup relayout regressions.

This fixes a major speed regression introduced in commit 64ff85b6925224d9a0a3 in PR #2521 (squashed into b2f44074) that made loading of big files and opening the keyboard incredibly slow and proportional to the file size.

* docs: add lrq3000 in CONTRIBUTORS.md
* fix: Refactor speed regression fix duplications into a function syncEditorMinHeightOnce() to do one-shot editor min-height sync to prevent large-file open slowdown + cover more cases as originally intended in 64ff85b6

Signed-off-by: Stephen L. <LRQ3000@gmail.com>

app/src/main/java/net/gsantner/markor/activity/DocumentEditAndViewFragment.java
@@ -232,14 +232,9 @@ public class DocumentEditAndViewFragment extends MarkorBaseFragment implements F
             });
         }
 
-        final Runnable ensureMinHeight = () -> _hlEditor.post(() -> {
-            final int height = _verticalScrollView.getHeight();
-            if (height > 0 && height != _hlEditor.getMinHeight()) {
-                _hlEditor.setMinHeight(height);
-            }
-        });
-        _verticalScrollView.addOnLayoutChangeListener((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> ensureMinHeight.run());
-        _verticalScrollView.post(ensureMinHeight);
+        // Keep this as a one-shot min-height sync. Do not use a persistent layout listener here,
+        // otherwise large documents trigger repeated relayout work during startup.
+        syncEditorMinHeightOnce(_verticalScrollView);
     }
 
     @Override
@@ -257,6 +252,10 @@ public class DocumentEditAndViewFragment extends MarkorBaseFragment implements F
 
         _hlEditor.recomputeHighlighting(); // Run before setting scroll position
         TextViewUtils.setSelectionAndShow(_hlEditor, startPos);
+
+        // One-shot floor for first render after content/highlighting setup.
+        // Do not replace with per-layout updates; they regress big-file open performance.
+        syncEditorMinHeightOnce(_editorHolder);
 
         // Fade in to hide initial jank
         _hlEditor.post(() -> _hlEditor.animate().alpha(1).setDuration(250).start());
@@ -680,6 +679,7 @@ public class DocumentEditAndViewFragment extends MarkorBaseFragment implements F
                 if (viewScroll != null) {
                     setMarginBottom(viewScroll, marginBottom);
                 }
+                syncEditorMinHeightOnce(_verticalScrollView);
             }
         }
     }
@@ -828,6 +828,18 @@ public class DocumentEditAndViewFragment extends MarkorBaseFragment implements F
         }
     }
 
+    private void syncEditorMinHeightOnce(final View parent) {
+        if (parent == null) {
+            return;
+        }
+        parent.post(() -> {
+            final int parentHeight = parent.getHeight();
+            if (parentHeight > 0 && parentHeight != _hlEditor.getMinHeight()) {
+                _hlEditor.setMinHeight(parentHeight);
+            }
+        });
+    }
+
     private void updateMenuToggleStates(final int selectedFormatActionId) {
         MenuItem mi;
         if ((mi = _fragmentMenu.findItem(R.id.action_wrap_words)) != null) {
@@ -895,6 +907,7 @@ public class DocumentEditAndViewFragment extends MarkorBaseFragment implements F
             }
 
             _hlEditor.requestLayout();
+            syncEditorMinHeightOnce(_editorHolder);
 
             _hlEditor.setHighlightingEnabled(hlEnabled);
             _hlEditor.post(() -> {

app/src/main/res/layout/document__fragment__edit.xml
@@ -48,7 +48,7 @@
 
                 <net.gsantner.markor.frontend.textview.HighlightingEditor
                     android:id="@+id/document__fragment__edit__highlighting_editor"
-                    android:layout_width="0dp"
+                    android:layout_width="match_parent"
                     android:layout_height="wrap_content"
                     android:layout_weight="1"
                     android:alpha="0"